23824754fba8c7b71995dbef396a690ecf9c4578
esusaki <susaki2002@gmail.com>
2024-09-15 13:33:36 +0900

    hoge

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function translate(){
-    text_area = document.getElementById('text_area');
-    const word = text_area.value;
-    alert(word);
-}
+//function translate(){
+ //   text_area = document.getElementById('text_area');
+ //   const word = text_area.value;
+ //   alert(word);
+//}
 
+const ok_button = document.getElementById('ok_button');
+
+ok_button.addEventListener(
+    "click",function(){
+        console.log('hey')
+    }
+)
 
 //kuromoji.builder({ dicPath: "dict" }).build(function (err, tokenizer) {
     // tokenizer is ready
