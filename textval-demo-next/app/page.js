0fded75d1e9c5cae4c25736ff11b6bdbae91f704
esusaki <susaki2002@gmail.com>
2024-10-10 13:21:00 +0900

    fixed og-image url

--- textval-demo-next/app/page.js
+++ textval-demo-next/app/page.js
@@ -45,7 +45,7 @@ export default function Home() {
           : <div style={{ "width": "50%","margin":"auto", "text-align": "center", "font-size": "2em", "color": "white", "font-weight": "bold", "margin-top": "20px" }}>
             {displayText}
               <div>
-                <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-text={'textvalでお祭り翻訳しました！'} data-url={encodeURI("https://ogp-test-omega.vercel.app/show-ogp/" + displayText)} data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-text={'textvalでお祭り翻訳しました！'} data-url={encodeURI("https://textval-demo.vercel.app/og/" + displayText)} data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
               </div>
           </div>
         }
